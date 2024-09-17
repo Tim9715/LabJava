d28c9667e35be5851d5303bdc621a5218b6d0e23
Tim <play-t@bk.ru>
2024-09-17 15:19:30 +0400

    First Program Java

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,17 @@
+import myfirstpackage.*;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        MyFirstPackage o = new MyFirstPackage(0, 0);
+        int i, j;
+        for (i = 1; i <= 8; i++) {
+            for (j = 1; j <= 8; j++) {
+                o.setFirstNumber(i);
+                o.setSecondNumber(j);
+                System.out.print(o.performOperation());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/myfirstpackage/MyFirstPackage.java
@@ -0,0 +1,31 @@
+package myfirstpackage;
+
+public class MyFirstPackage {
+    private int firstNumber;
+    private int secondNumber;
+
+    public MyFirstPackage(int var1, int var2) {
+        this.firstNumber = var1;
+        this.secondNumber = var2;
+    }
+
+    public int getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public void setFirstNumber(int var1) {
+        this.firstNumber = var1;
+    }
+
+    public int getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setSecondNumber(int var1) {
+        this.secondNumber = var1;
+    }
+
+    public int performOperation() {
+        return this.firstNumber * this.secondNumber;
+    }
+}
